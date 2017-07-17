5f586b887751f8c15a23b11d33235b7cdf920161
wang <834603810@qq.com>
2017-07-17 15:33:07 +0800

    first

---
+++ advanced/backend/controllers/AccountController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * 会员管理
+ * Class AccountController
+ * @package backend\controllers
+ * @auth Mr Wang
+ * @date 2017-07-17
+ */
+class AccountController extends CommonController
+{
+
+
+	public function actionLogin()
+	{
+		echo "1";die;
+	}
+
+	
+}
+
+
+?>
---
+++ advanced/backend/views/layouts/three.php
@@ -0,0 +1,4 @@
+<?php include "./three/header.php" ?>
+<?php include "./three/left.php" ?>
+<?= $content ?>
+<?php include "./three/footer.php" ?>
